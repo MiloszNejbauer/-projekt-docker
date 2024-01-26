c26cc0a58f25583295c9dc615fcaf7e2301ad9d4
MiloszNejbauer <157845913+MiloszNejbauer@users.noreply.github.com>
2024-01-26 22:14:46 +0100

    Add files via upload

---
+++ frontend/src/components/GameComponents/Snake.jsx
@@ -0,0 +1,5 @@
+
+export const Snake = ({ key }) => {
+    return <div key={key} className="snake"></div>;
+  };
+
---
+++ frontend/src/components/UserComponents/UserModals.jsx
@@ -0,0 +1,11 @@
+import {AddUserModal} from "./AddUserModal";
+import {ViewUserModal} from "./ViewUserModal";
+
+export const UserModals = () => {
+    return (
+        <>
+            <AddUserModal />
+            <ViewUserModal />
+        </>
+    )
+}
